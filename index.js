e333c68b28292bb80634355553e395c3709f4f8e
matthewxvoorhees <matthew.crnich@gmail.com>
2020-12-02 15:42:52 -0700

    commit tree d9906c33c009f017b28a1650876f5869522f7c49

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+return(`This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(thing="go to the office"){
+return (`This Monday, I will ${thing}.`)
+}
+
+function wrapAdjective(fancy="*"){
+    return function(adjective="special") {
+        return `You are ${fancy}${adjective}${fancy}!`
+    }
+}
+
+const Calculator = {
+    add (a, b){
+    return a + b
+    },
+    subtract (a, b){
+    return a - b
+    },
+    multiply (a, b){
+    return a * b
+    },
+    divide (a, b){
+    return a / b
+    }  
+}
+
+function actionApplyer(start, funArr) {
+    let num = start
+    for (const fun of funArr) {
+        num = fun(num)
+    }
+    return num
+}
